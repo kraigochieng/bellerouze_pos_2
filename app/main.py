ea0de5019a95a9627aff63077b9c4c74eaa7a89d
kraigochieng <kraigochieng@gmail.com>
2024-06-10 06:31:56 +0300

    separated backend api from frontent api

--- app/main.py
+++ app/main.py
@@ -1,18 +1,18 @@
 from fastapi import FastAPI
-from .routers import item, item_category, item_with_category
+from .routers import api__item, api__item_category, api__item_with_category
 from .database import engine
 from .models import Base
+
 # Create FastAPI instance
 app = FastAPI()
-
 
 # Create the database tables
 Base.metadata.create_all(bind=engine)
 
 # Prefix the routers and register them
-app.include_router(item.router, prefix="/api/items", tags=["items"])
-app.include_router(item_category.router, prefix="/api/item_categories", tags=["item categories"])
-app.include_router(item_with_category.router, prefix="/api/item_with_categories", tags=["items with categories"])
+app.include_router(api__item.router, prefix="/api/items", tags=["items"])
+app.include_router(api__item_category.router, prefix="/api/item_categories", tags=["item categories"])
+app.include_router(api__item_with_category.router, prefix="/api/item_with_categories", tags=["items with categories"])
 
 
 # Optional: Define a root route
