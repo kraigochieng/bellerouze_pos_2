dc429f928d2b4af69bd732f8143116546cd0f34e
kraigochieng <kraigochieng@gmail.com>
2024-06-23 12:25:31 +0300

    added SQLAlchemy serializer to requirements.txt

--- app/routers/test_api__item_category.py
+++ app/routers/test_api__item_category.py
@@ -1,6 +1,7 @@
 from fastapi.testclient import TestClient
 
-from ..schemas import ItemCategory
+from ..schemas import ItemCategory as ItemCategorySchema
+from ..models import ItemCategory as ItemCategoryModel
 from .api__item_category import (
     router,
 )
@@ -9,19 +10,20 @@ client = TestClient(router)
 
 
 fake_db = [
-    ItemCategory(id=1, name="1"),
-    ItemCategory(id=2, name="2"),
-    ItemCategory(id=3, name="3"),
+    ItemCategorySchema(id=1, name="1"),
+    ItemCategorySchema(id=2, name="2"),
+    ItemCategorySchema(id=3, name="3"),
 ]
 
 
 def test_create_item_category():
     response = client.post("/", json=fake_db[1].model_dump())
     assert response.status_code == 200
-    assert response.json() == fake_db[1].model_dump()
+    model = ItemCategoryModel(**fake_db[1].model_dump())    
+    assert ItemCategoryModel(**response.json()) == model
 
 
 def test_read_item_categories():
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == [x.model_dump() for x in fake_db]
+    assert response.json() == [ItemCategoryModel(**x.model_dump()) for x in fake_db]
