4c47e3ce526fcc8cd94f53c8851fb3888f1e0705
kraigochieng <kraigochieng@gmail.com>
2024-06-26 23:08:02 +0300

    simplified schemas

--- app/schemas.py
+++ app/schemas.py
@@ -1,54 +1,50 @@
+from typing import Optional
+
 from pydantic import BaseModel, ConfigDict
 
 
-class ItemBase(BaseModel):
+class ItemCreate(BaseModel):
     name: str
     description: str
 
 
-class ItemCreate(ItemBase):
-    pass
-
-
-class ItemUpdate(ItemBase):
-    pass
-
-
-class Item(ItemBase):
-    model_config = ConfigDict(extra="ignore", from_attributes=True)
-
+class ItemRead(BaseModel):
     id: int
+    name: str
+    description: str
 
 
-class ItemCategoryBase(BaseModel):
+class ItemUpdate(BaseModel):
+    name: Optional[str]
+    description: Optional[str]
+
+
+class ItemCategoryCreate(BaseModel):
     name: str
 
 
-class ItemCategoryCreate(ItemCategoryBase):
-    pass
-
-
-class ItemCategoryUpdate(ItemCategoryBase):
-    pass
-
-
-class ItemCategory(ItemCategoryBase):
-    model_config = ConfigDict(extra="ignore", from_attributes=True)
-
+class ItemCategoryRead(BaseModel):
     id: int
+    name: str
 
 
-class ItemWithCategoryBase(BaseModel):
+class ItemCategoryUpdate(BaseModel):
+    name: Optional[str]
+
+
+class ItemWithCategoryCreate(BaseModel):
     item_id: int
     item_category_id: int
 
 
-class ItemWithCategoryCreate(ItemWithCategoryBase):
-    pass
+class ItemWithCategoryRead(BaseModel):
+    item_id: int
+    item_category_id: int
 
 
-class ItemWithCategoryUpdate(ItemWithCategoryBase):
-    pass
+class ItemWithCategoryUpdate(BaseModel):
+    item_id: Optional[int]
+    item_category_id: Optional[int]
 
 
 class ItemWithCategory(ItemWithCategoryBase):
