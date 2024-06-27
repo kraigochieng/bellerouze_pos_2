d82bf2d113487e5daf4d4e7cc69c5caf1e8f9f30
kraigochieng <kraigochieng@gmail.com>
2024-06-27 20:41:13 +0300

    adding the mock test instances

--- app/conftest.py
+++ app/conftest.py
@@ -1,10 +1,29 @@
 import pytest
-from .database import Base, test_engine
+from .database import get_db
+from .main import app
+from unittest.mock import MagicMock
+from fastapi.testclient import TestClient
 
-@pytest.fixture(scope="module", autouse=True)
-def setup_and_teardown():
-    # Create tables before tests
-    Base.metadata.create_all(bind=test_engine)
-    yield
-    # Clean up tables after tests
-    Base.metadata.drop_all(bind=test_engine)
+@pytest.fixture()
+def override_get_db():
+    mock_db_session = MagicMock()    
+    yield mock_db_session
+    mock_db_session.close()
+
+@pytest.fixture()
+def client(override_get_db):
+    app.dependency_overrides[get_db] = lambda: override_get_db
+    with TestClient(app) as client:
+        yield client
+
+# @pytest.fixture(scope="module", autouse=True)
+# def setup_and_teardown():
+#     app.dependency_overrides[get_db] = override_get_db
+#     # Create tables before tests
+#     # Base.metadata.create_all(bind=test_engine)
+#     yield
+#     # Clean up tables after tests
+#     # Base.metadata.drop_all(bind=test_engine)
+
+
+# app.dependency_overrides[get_db] = lambda: override_get_db
